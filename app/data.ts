70a5b9220d6aaeaaf02774a7db2a0aa579c2af95
Matt Whitney <matt@bedda.tech>
2026-03-06 15:38:51 +0100

    add pizza data and API endpoint

---
+++ app/data.ts
@@ -0,0 +1,51 @@
+export type Pizza = {
+  id: number;
+  name: string;
+  price: number;
+  toppings: string[];
+};
+
+export const pizzas: Pizza[] = [
+  {
+    id: 1,
+    name: "Margherita",
+    price: 899,
+    toppings: ["Marinara", "Mozzarella", "Basil"],
+  },
+  {
+    id: 2,
+    name: "Pepperoni",
+    price: 999,
+    toppings: ["Marinara", "Mozzarella", "Pepperoni"],
+  },
+  {
+    id: 3,
+    name: "BBQ Chicken",
+    price: 1099,
+    toppings: ["Marinara", "Mozzarella", "BBQ Chicken"],
+  },
+  {
+    id: 4,
+    name: "Hawaiian",
+    price: 1199,
+    toppings: ["Marinara", "Mozzarella", "Hawaiian"],
+  },
+  {
+    id: 5,
+    name: "Vegetarian",
+    price: 1299,
+    toppings: ["Marinara", "Mozzarella", "Vegetarian"],
+  },
+  {
+    id: 6,
+    name: "Meat Lover",
+    price: 1399,
+    toppings: ["Marinara", "Mozzarella", "Meat Lover"],
+  },
+  {
+    id: 7,
+    name: "Four Cheese",
+    price: 1499,
+    toppings: ["Marinara", "Mozzarella", "Four Cheese"],
+  },
+];
